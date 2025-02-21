53b4480b300034d3e7e0a6c492deb6c574b9ef92
Rohamunira <mkk88aytax2cayfazt8@gmail.com>
2025-02-21 19:14:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from playwright.sync_api import sync_playwright 
 import cv2
 import numpy
 import matplotlib
@@ -10,3 +11,12 @@ img = img.convert("RGB") #
 pixels = img.getdata()
 for color in pixels:
     rgb2hex = "#{:02x}{:02x}{:02x}".format(color[0], color[1], color[2])
+with sync_playwright() as p:
+    browser = p.chromium.launch(
+        headless = False, # display the chromium windows 
+        executable_path = "D:/program files/chrome-win/chrome.exe", # custom executable chromium
+        timeout = 30000
+    )
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://rplace.live/")
